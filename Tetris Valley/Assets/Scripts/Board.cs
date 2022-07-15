63ab84aaa29b1f6a85d24a1dee12fd9b79e28a53
dairuidenny <835399659@qq.com>
2022-07-15 21:52:34 +0800

    新建目录

---
+++ Tetris Valley/Assets/Scripts/Board.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public TetrominoData[] tetrominoes;
+
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+
+        for (int i=0; i<this.tetrominoes.Length; i++)
+        {
+            this.tetrominoes[i].Initialsze();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominoes.Length);
+        TetrominoData data = this.tetrominoes[random];
+    }
+
+    public void Set(Piece piece)
+    {
+        for(int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
+}
